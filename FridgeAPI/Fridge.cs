92030c7755697c1db95c211f2472f387139306d9
Aleksas Legačinskas <legacinskas@gmail.com>
2019-05-13 20:39:42 +0300

    Implement FridgeAPI

--- FridgeAPI/Fridge.cs
+++ FridgeAPI/Fridge.cs
@@ -1,13 +1,38 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
 
 namespace FridgeAPI
 {
-    public static class Fridge
+    public class Fridge : IFridge
     {
-        public static string GetFrtidgeContents(Guid fridgeId, string token)
+        private string[] products = { "Milk", "Eggs", "Tomatoes" };
+        private int[] deltas = { -1, -2, -3, 5 };
+        private Random r;
+
+        public IEnumerable<KeyValuePair<string, int>> GetActivityLog(Guid fridgeId)
         {
-            // TODO: Add synchronisation
-            throw new NotImplementedException();
+            r = new Random(fridgeId.GetHashCode());
+            
+            Dictionary<string, int> current = new Dictionary<string, int>{
+                { "Milk", 0 },
+                { "Eggs", 0 },
+                { "Tomatoes", 0 }
+            };
+
+            for(int i = 0; i < 15; i++)
+            {
+                string item = products[r.Next(products.Length)];
+
+                int[] goodDeltas = deltas.Where(d => current[item] + d >= 0).ToArray();
+
+                int delta = goodDeltas[r.Next(goodDeltas.Length)];
+
+                current[item] += delta;
+
+                yield return new KeyValuePair<string, int>(item, delta);
+            }
         }
     }
 }
---
+++ FridgeAPI/IFridge.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FridgeAPI
+{
+    public interface IFridge
+    {
+        IEnumerable<KeyValuePair<string,int>> GetActivityLog(Guid fridgeId);
+    }
+}
